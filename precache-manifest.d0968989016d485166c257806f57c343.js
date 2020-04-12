286ab636765a1e82240495ac21c3f2f5eea42747
Aru <alejandro.diazpallas@gmail.com>
2020-04-12 19:12:26 +0200

    Updates

---
+++ precache-manifest.d0968989016d485166c257806f57c343.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0099d8e6dd9e1cbb38813bb55346da7",
+    "url": "/kitties-rolodex/index.html"
+  },
+  {
+    "revision": "da5da4922a2dd781b957",
+    "url": "/kitties-rolodex/static/css/main.99053296.chunk.css"
+  },
+  {
+    "revision": "ab3f317ba6f480ff1aa1",
+    "url": "/kitties-rolodex/static/js/2.7a6e619d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/kitties-rolodex/static/js/2.7a6e619d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "da5da4922a2dd781b957",
+    "url": "/kitties-rolodex/static/js/main.4765ab20.chunk.js"
+  },
+  {
+    "revision": "5e5c7d78e4a0f20386dc",
+    "url": "/kitties-rolodex/static/js/runtime-main.0684aeac.js"
+  }
+]);
